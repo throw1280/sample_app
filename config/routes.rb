9b78343dc93b1d2725a8bbc47413010564653c6d
dbvn1280 <dbvn1280@gmail.com>
2019-04-23 22:05:56 -0400

    Chapter_3 Mostly static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  scope "(:locale)", locale: /en|vi/ do
+    resources :static_pages
+    get "help" => "static_pages#help"
+    get "about" => "static_pages#about"
+    get "contact" => "static_pages#contact"
+    root "static_pages#home"
+  end
 end
