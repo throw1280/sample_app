7eac68a1d8876f9359276e2c8875ea529cc21726
dbvn1280 <dbvn1280@gmail.com>
2019-05-05 00:15:05 -0400

    Add a full_title helper

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
 
   scope "(:locale)", locale: /en|vi/ do
-    resources :static_pages
     get "help" => "static_pages#help"
     get "about" => "static_pages#about"
     get "contact" => "static_pages#contact"
