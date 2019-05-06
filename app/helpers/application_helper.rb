09fea845ea026c09642554e66d4053cd44d059df
dbvn1280 <dbvn1280@gmail.com>
2019-05-06 02:35:53 -0400

    Finish layout and routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,6 @@
 module ApplicationHelper
-
-  def full_title (page_title = "")
-    base_title = "Ruby on Rails Tutorial Sample App"
+  def full_title page_title = ""
+    base_title = t("title_app")
     if page_title.empty?
       base_title
     else
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
 
   scope "(:locale)", locale: /en|vi/ do
-    resources :static_pages
     get "help" => "static_pages#help"
     get "about" => "static_pages#about"
     get "contact" => "static_pages#contact"
+    get "signup" => "users#new"
     root "static_pages#home"
   end
 end
