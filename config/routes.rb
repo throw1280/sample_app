ed19b324804f5d8be0218f976cfcf6bd3cdd4853
dbvn1280 <dbvn1280@gmail.com>
2019-05-07 02:28:11 -0400

    Use SSL and the Puma webserver in production

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
 
   get 'users/new'
   scope "(:locale)", locale: /en|vi/ do
-    resources :static_pages
     get "help" => "static_pages#help"
     get "about" => "static_pages#about"
     get "contact" => "static_pages#contact"
