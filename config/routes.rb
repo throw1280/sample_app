6c633a5ad944a3ff7bfaeff6b1db22a0e238cefc
dbvn1280 <dbvn1280@gmail.com>
2019-05-14 00:49:29 -0400

    Chapter 6: Make a basic User model

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     get "help" => "static_pages#help"
     get "about" => "static_pages#about"
     get "contact" => "static_pages#contact"
+  resource: users
     get "signup" => "users#new"
     root "static_pages#home"
   end
